d2646d1d73cb73a0169e57bc1ab2863dafad18a9
sunb001 <2267660581@qq.com>
2018-06-22 14:49:00 +0800

    slide mouse down

--- app/web/view/hamster/components/slide/Slide.js
+++ app/web/view/hamster/components/slide/Slide.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import CSSModules from 'react-css-modules';
+import {connect} from 'react-redux'
 
 import SlideComponent from './Component';
 import Block from '../block'
+import blockActions from '../../actions/slide';
+import styles from './Slide.scss';
 
+const mapDispatchToProps = dispatch => {
+    return {
+        handleMouseDown: () => dispatch(blockActions.slideMouseDown())
+    }
+}
+
+@connect(null, mapDispatchToProps)
+@CSSModules(styles)
 class Slide extends React.Component {
     static displayName = 'Slide';
 
     static Component = SlideComponent;
     
+    handleMouseDown = (e) => {
+        if (!e.target.classList.contains('cj-slide')) return;
+
+        const {handleMouseDown} = this.props;
+        handleMouseDown && handleMouseDown();
+    }
+
     render() {
         return (
-            <SlideComponent {...this.props} Block={Block} />
+            <div styleName='slide-wrap' className='slide-wrap cj-slide' onMouseDown={this.handleMouseDown} >
+                <SlideComponent {...this.props} Block={Block} />
+            </div>
         );
     }
 }
