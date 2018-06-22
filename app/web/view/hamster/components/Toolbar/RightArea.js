a8dc352dbbadc5da4d9764fdc4f0861a9f869ece
sunb001 <2267660581@qq.com>
2018-06-22 16:40:37 +0800

    组合

--- app/web/view/hamster/components/Toolbar/RightArea.js
+++ app/web/view/hamster/components/Toolbar/RightArea.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Button} from 'antd'
 
-export const RightArea = (props) => {
-    return (
-        <div className='right-area'>
-            右边区域
-        </div>
-    )
+import { withHamster } from '../../manager';
+
+@withHamster()
+class RightArea extends React.Component {
+    handleUnite = () => {
+        const {hamster} = this.props;
+        hamster.blockManager.groupUnite()
+    }
+
+    render() {
+        return (
+            <div className='right-area'>
+                右边区域
+                <Button onClick={this.handleUnite}>
+                    合并元素
+                </Button>
+            </div>
+        )
+    }
 }
 
 RightArea.propTypes = {
 
-}
+};
 
+export {RightArea}
 export default RightArea;
